4eddcc82461eeca4cd9aa5e4936fcaeebfd7862c
fozilovaweb <zaynabxon23@gmail.com>
2025-09-07 11:24:08 +0500

    still

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <Title />
-      <div className="W-full max-w-[57.5rem] flex  flex-col sm:flex-row  items-center gap-8   rounded-t-3xl p-8 bg-white">
+      <div className="W-full max-w-[57.5rem] flex  flex-col sm:flex-row  items-center gap-8   rounded-t-3xl sm:rounded-3xl p-8 bg-white">
         <Form />
         <ResultCard />
       </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,7 +6,7 @@ function Form() {
   const [values, setValues] = useState({ bill: 0, people: 0, tip: 0 });
   console.log(values);
   return (
-    <div className="w-72 sm:w-80 md:95 flex flex-col gap-8  sm:gap-3 ">
+    <div className="w-72 sm:w-80 md:95 flex flex-col gap-8  sm:gap-3  ">
       <Input label={"Bill"} icon={"dollar"} setValues={setValues} />
       <SelectTip setValues={setValues} />
       <Input label={"Number of People"} icon={"user"} setValues={setValues} />
